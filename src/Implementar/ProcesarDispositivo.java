46f238367df2ca81d3d2e781334b53ce64769b15
José Alexander <josehuarialva@gmail.com>
2024-10-21 22:11:21 -0500

    EjemploPrimerGuardado

---
+++ src/Implementar/ProcesarDispositivo.java
@@ -0,0 +1,7 @@
+package Implementar;
+public class ProcesarDispositivo {
+    public static void main(String[] args) {
+        Formulario form=new Formulario();
+        form.setVisible(true);
+    }
+}
